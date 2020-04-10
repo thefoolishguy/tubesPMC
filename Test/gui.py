b41f4338ebf47bec0de1e4d1dc5de699409dc981
unknown <matthewryo@gmail.com>
2020-04-10 14:18:41 +0700

    update gui

--- Test/gui.py
+++ Test/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 # make a new window
 win = tk.Tk()
@@ -8,21 +9,27 @@ win.title("Testing GUI")
 screen_x = win.winfo_screenwidth()
 screen_y = win.winfo_screenheight()
 # win.geometry("{0}x{1}+0+0".format(screen_x, screen_y))
-win.geometry("800x600")
+win.geometry("1278x609")
 win.resizable(False, False)
-# make the window zoomed (max size, fit to screen)
-# win.state("zoomed")
+
 # insert background image name to a variable
-background_image = tk.PhotoImage(file="rangkaian_tubes.png")
+im = Image.open("rangkaian_tubes.png")
+im = im.resize((1278, 609))
+background_image = ImageTk.PhotoImage(im)
+
 # make a label for the image
 background_label = tk.Label(win, image=background_image)
+
+
 # put the image label at the corner left
-# background_label.place(x=0, y=0, relwidth=1, relheight=1)
-background_label.pack(fill="both", expand="yes")
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+# background_label.pack(fill="both", expand="yes")
+
+
 # create a text label
 label1 = tk.Label(win, text="Screen width = {}".format(screen_x), pady=5)
 # put the label using pack
-label1.pack(side="top", expand="yes", fill="both")
+label1.pack(side="top")
 label2 = tk.Label(win, text="Screen height = {}".format(screen_y), pady=5)
 label2.pack(side="top")
 # loop the window until closed
