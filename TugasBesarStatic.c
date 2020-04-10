4ec2bf4ca387ee0adf56b34abe969b651be8608f
Agung Dwi Laksana <47895911+AgungDwiL@users.noreply.github.com>
2020-04-10 22:33:42 +0700

    Add files via upload

--- TugasBesarStatic.c
+++ TugasBesarStatic.c
@@ -18,154 +18,106 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main (void)
+void getTracking(double V, double R1, double R2, double R3, double R4, double C, int ModeSimulasi, double tMax, double inc)
 {
     /* DEKLARASI VARIABEL */
-    double V, R1, R2, R3, R4, C; // input nilai komponen (SI)
     double Tau; //koefisien waktu
-    double *t, tMax;   // waktu
-    double inc; // increment waktu
-    double *V2, *V3, *V4, *V5, *I1, *I2, *I3, *I4, *Ic; // output karakteristik rangkaian (SI)
-    char ModeSimulasi[6]; // (OP/TRANS)
+    double t;    // waktu
+    double V1, V2, V3, V4, I1, I2, I3, I4, Ic; // output karakteristik rangkaian (SI)
     int i; // index
 
     /* ALGORITMA */
-    // input besaran
-    printf("PROGRAM SIMULASI RANGKAIAN\n");
-    printf("Masukan input :\n");
-    printf("V = ");
-    scanf("%lf", &V);
-    printf("R1 = ");
-    scanf("%lf", &R1);
-    printf("R2 = ");
-    scanf("%lf", &R2);
-    printf("R3 = ");
-    scanf("%lf", &R3);
-    printf("R4 = ");
-    scanf("%lf", &R4);
-    printf("C = ");
-    scanf("%lf", &C);
 
     // input mode simulasi
-    printf("\n");
-    printf("Masukan mode simulasi (OP/TRANS) : ");
-    scanf(" %s", ModeSimulasi);
         // Mode OP (kapasitor open circuit)
-    if (!strcmp(ModeSimulasi, "OP"))
+    if (ModeSimulasi == 1)
     {
-        // Alokasi memori
-        V2 = (double*)malloc(sizeof(double));
-        V3 = (double*)malloc(sizeof(double));
-        V4 = (double*)malloc(sizeof(double));
-        V5 = (double*)malloc(sizeof(double));
-        I1 = (double*)malloc(sizeof(double));
-        I2 = (double*)malloc(sizeof(double));
-        I3 = (double*)malloc(sizeof(double));
-        I4 = (double*)malloc(sizeof(double));
-        Ic = (double*)malloc(sizeof(double));
 
         // Perhitungan
-        *V5 = V;
-        *V2 = ((R3 + R4)/(R1 + R3 + R4))*V;
-        *V4 = *V2;
-        *V3 = (R4/(R1 + R3 + R4))*V;
-        *I2 = 0;
-        *Ic = 0;
-        *I1 = V / (R1 + R3 + R4);
-        *I3 = *I1;
-        *I4 = *I1;
-
-        // Cetak Besaran
-        printf("\nHasil Simulasi OP diperoleh : \n");
-        printf("V2 = %.4f Volt\n", *V2);
-        printf("V3 = %.4f Volt\n", *V3);
-        printf("V4 = %.4f Volt\n", *V4);
-        printf("V5 = %.4f Volt\n", *V5);
-
-        printf("I1 = %.4f Amp\n", *I1);
-        printf("I2 = %.4f Amp\n", *I2);
-        printf("I3 = %.4f Amp\n", *I3);
-        printf("I4 = %.4f Amp\n", *I4);
-        printf("Ic = %.4f Amp\n", *Ic);
-
-    } else if (!strcmp(ModeSimulasi,"TRANS"))
-    {  // Mode Transiens
-
-       // Input parameter waktu
-       printf("Masukan parameter waktu : \n");
-       printf("t Max      (s) = ");
-       scanf("%lf", &tMax);
-       printf("t increment(s) = ");
-       scanf("%lf", &inc);
-
-       // Hitung panjang alokasi memori
-       int nElemen = ((int)(tMax/inc)+1);
-
-       // Inisiasi Alokasi memory
-        t  = (double*)malloc(nElemen*sizeof(double));
-        V2 = (double*)malloc(nElemen*sizeof(double));
-        V3 = (double*)malloc(nElemen*sizeof(double));
-        V4 = (double*)malloc(nElemen*sizeof(double));
-        V5 = (double*)malloc(nElemen*sizeof(double));
-        I1 = (double*)malloc(nElemen*sizeof(double));
-        I2 = (double*)malloc(nElemen*sizeof(double));
-        I3 = (double*)malloc(nElemen*sizeof(double));
-        I4 = (double*)malloc(nElemen*sizeof(double));
-        Ic = (double*)malloc(nElemen*sizeof(double));
-
-        // Perhitungan
-        Tau = (((R1 * (R3 + R4))/(R1 + R3 + R4)) + R2)*C;
-
-        // Tracking Loop
-        for (i = 0; i < nElemen+1; i++)
-        {
-            // Calculate time
-            t[i] = i*inc;
-
-            //Calculate value every point
-            V4[i] = ((R3 + R4)/(R1 + R3 + R4))*V*(1 - (exp(-1*(t[i]/Tau))));
-            Ic[i] = (C/Tau)*((R3 + R4)/(R1 + R3 + R4))*V*(exp(-1*(t[i]/Tau)));
-            V2[i] = (Ic[i]*R2) + V4[i];
-            I3[i] = V2[i]/(R3 + R4);
-            I4[i] = I3[i];
-            V3[i] = I4[i]*R4;
-            I1[i] = Ic[i] + I3[i];
-            I2[i] = Ic[i];
-            V5[i] = V;
-
-        }
-
-
-        // Mencetak ke dan File
+        V1 = V;
+        V2 = ((R3 + R4)/(R1 + R3 + R4))*V;
+        V4 = V2;
+        V3 = (R4/(R1 + R3 + R4))*V;
+        I2 = 0;
+        Ic = 0;
+        I1 = V / (R1 + R3 + R4);
+        I3 = I1;
+        I4 = I1;
 
         // Open File External
         FILE* fp;
         fp = fopen("HasilTracking.txt", "w");
         printf("\nMencetak ke File...\n");
+        fprintf(fp,"HASIL SIMULASI OP\n");
+        fprintf(fp,"time \tV1 \tV2 \tV3 \tV4 \tI1 \tI2 \tI3 \tI4 \tIc\n");
+
+        // Print value
+        fprintf(fp, "%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f\n",
+                    t, V1, V2, V3, V4, I1, I2, I3, I4, Ic);
+
+        // Close File External
+        fclose(fp);
+
+    } else if (ModeSimulasi == 2)
+    {  // Mode Transiens
+
+       // Hitung panjang alokasi memori
+       int nElemen = ((int)(tMax/inc)+1);
+
+        // Perhitungan
+        Tau = (((R1 * (R3 + R4))/(R1 + R3 + R4)) + R2)*C;
+
+        // Open File External
+        FILE* fp;
+        fp = fopen("HasilTracking.txt", "w");
+        printf("Mencetak ke File...\n");
         fprintf(fp,"HASIL TRACKING BESARAN LISTIK\n");
-        fprintf(fp,"time \tV2 \tV3 \tV4 \tV5 \tI1 \tI2 \tI3 \tI4 \tIc\n");
+        fprintf(fp,"time \tV1 \tV2 \tV3 \tV4 \tI1 \tI2 \tI3 \tI4 \tIc\n");
 
-        for(i = 0; i < nElemen+1; i++)
+        // Tracking Loop
+        for (i = 0; i < nElemen+1; i++)
         {
-            fprintf(fp, "%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f\n",
-                    t[i], V2[i], V3[i], V4[i], V5[i], I1[i], I2[i], I3[i], I4[i], Ic[i]);
+            // Calculate time
+            t = i*inc;
 
+            //Calculate value every point
+            V4 = ((R3 + R4)/(R1 + R3 + R4))*V*(1 - (exp(-1*(t/Tau))));
+            Ic = (C/Tau)*((R3 + R4)/(R1 + R3 + R4))*V*(exp(-1*(t/Tau)));
+            V2 = (Ic*R2) + V4;
+            I3 = V2/(R3 + R4);
+            I4 = I3;
+            V3 = I4*R4;
+            I1 = Ic + I3;
+            I2 = Ic;
+            V1 = V;
+
+            fprintf(fp, "%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f \t%.4f\n",
+                    t, V1, V2, V3, V4, I1, I2, I3, I4, Ic);
         }
+
         printf("Pencetakan selesai");
 
         fclose(fp);
-    }
 
-    // Hapus Memori
-        free(V2);
-        free(V3);
-        free(V4);
-        free(V5);
-        free(I1);
-        free(I2);
-        free(I3);
-        free(I4);
-        free(Ic);
+    } else
+    {
+        printf("INPUT SALAH !");
+    }
+}
+
+int main (void)
+{
+    double V  = 1;
+    double R1 = 1;
+    double R2 = 1;
+    double R3 = 1;
+    double R4 = 1;
+    double C  = 1;
+    int ModeSimulasi = 2;
+    double tMax = 1;
+    double inc = 0.1;
+
+    getTracking(V, R1, R2, R3, R4, C, ModeSimulasi, tMax, inc);
 
     return 0;
 }
